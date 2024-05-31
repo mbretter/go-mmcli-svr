58024442df8cd8f471bd46912e49b060b5dbab3b
Michael Bretterklieber <m.bretterklieber@simpko.com>
2024-05-31 09:18:05 +0200

    test exec modem

--- backend/mmcli/mmcli_test.go
+++ backend/mmcli/mmcli_test.go
@@ -70,3 +70,61 @@ func TestMmcli_Exec(t *testing.T) {
 		})
 	}
 }
+
+func TestMmcli_ExecModem(t *testing.T) {
+	tests := []struct {
+		name          string
+		modem         string
+		args          []string
+		expected      []byte
+		expectedErr   error
+		execArgs      []any
+		execReturn    []byte
+		execReturnErr error
+	}{
+		{
+			name:       "Success with modem",
+			modem:      "0",
+			args:       []string{"arg1", "arg2"},
+			expected:   []byte("Success"),
+			execArgs:   []any{"-J", "-m", "0", "arg1", "arg2"},
+			execReturn: []byte("Success"),
+		},
+		{
+			name:       "Success without modem",
+			modem:      "",
+			args:       []string{"arg1", "arg2"},
+			expected:   []byte("Success"),
+			execArgs:   []any{"-J", "-m", "/org/freedesktop/ModemManager1/Modem/0", "arg1", "arg2"},
+			execReturn: []byte("Success"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockExec := NewExecCommandMock(t)
+			if tt.modem == "" {
+				mockOutput := NewExecCommandOutputMock(t)
+				mockExec.EXPECT().Execute("bash", "-c", "mmcli -L -J | jq -r '.\"modem-list\"[0]'").Return(mockOutput)
+				mockOutput.EXPECT().Output().Return([]byte("/org/freedesktop/ModemManager1/Modem/0"), tt.execReturnErr)
+			}
+
+			mockOutput := NewExecCommandOutputMock(t)
+			mockExec.EXPECT().Execute("mmcli", tt.execArgs...).Return(mockOutput)
+			mockOutput.EXPECT().Output().Return(tt.execReturn, tt.execReturnErr)
+
+			cli := Provide()
+			cli.exec = mockExec.Execute
+
+			got, err := cli.ExecModem(tt.modem, tt.args...)
+
+			if tt.expectedErr == nil {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expected, got)
+			} else {
+				assert.EqualError(t, err, tt.expectedErr.Error())
+			}
+		})
+	}
+}
