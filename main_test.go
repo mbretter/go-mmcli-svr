fe9b551c1f11753009c3f3cfba9b4882a23be2b8
Michael Bretterklieber <mbretter@users.noreply.github.com>
2024-06-01 10:08:30 +0200

    add kinda dumb test

--- main_test.go
+++ main_test.go
@@ -3,31 +3,27 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/mbretter/go-mmcli-svr/backend"
-	"net/http"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRegisterSmsRoutes(t *testing.T) {
 	backendMock := backend.NewBackendMock(t)
+	r := chi.NewRouter()
+	registerSmsRoutes(r, backendMock)
 
-	tests := []struct {
-		name           string
-		method         string
-		path           string
-		expectedStatus int
-	}{
-		{"Get All Sms", http.MethodGet, "/sms/", http.StatusOK},
-		{"Get Sms By ID", http.MethodGet, "/sms/1", http.StatusOK},
-		{"Send Sms", http.MethodPost, "/sms", http.StatusOK},
-		{"Delete Sms", http.MethodDelete, "/sms/1", http.StatusOK},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := chi.NewRouter()
-			registerSmsRoutes(r, backendMock)
-
-			r.Routes()
-		})
+	routes := r.Routes()
+	assert.Len(t, routes, 3)
+	for _, route := range routes {
+		switch route.Pattern {
+		case "/sms/":
+			assert.Len(t, route.Handlers, 1)
+		case "/sms/{id:[a-zA-Z0-9%/]+}":
+			assert.Len(t, route.Handlers, 2)
+		case "/sms":
+			assert.Len(t, route.Handlers, 1)
+		default:
+			t.Errorf("Unexpected route: %s", route.Pattern)
+		}
 	}
 }
